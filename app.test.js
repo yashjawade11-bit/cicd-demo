5ca5b68005b26655c913b8090fc7f6fdebbdc428
yash jawade <yashjawade429@gmail.com>
2026-03-15 14:04:40 +0530

    update message

--- app.test.js
+++ app.test.js
@@ -1,6 +1,3 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Pipeline is LIVE! 🚀'));
-
-app.listen(3000, () => console.log('App running on port 3000'));
+app.get('/', (req, res) => {
+  res.send('Hello CI/CD Pipeline Updated!');
+});
